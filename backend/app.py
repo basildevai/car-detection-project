778f01aeb108f0e3fd546b2a36d3fbd1606fa41c
basildevai <basildevai@gmail.com>
2025-05-25 16:28:08 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -11,7 +11,9 @@ app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024
 CORS(app)  # Enable CORS for potential separate frontend hosting
 
 # Load model directly
-bbox_model = load_model('backend/mobilenet_bbox_model.h5')
+# bbox_model = load_model('backend/mobilenet_bbox_model.h5')
+# Load model with custom objects
+bbox_model = load_model('mobilenet_bbox_model.h5', custom_objects={'mse': tensorflow.keras.losses.MeanSquaredError()})
 
 def process_image(image):
     img = cv2.resize(image, (224, 224))
