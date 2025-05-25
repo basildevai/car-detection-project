fb1a0695d4d063100c76130556f711823530e187
basildevai <basildevai@gmail.com>
2025-05-25 16:30:15 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -42,5 +42,5 @@ def serve_frontend():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=False)
